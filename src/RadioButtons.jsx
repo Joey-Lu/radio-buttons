2974d0b290c351bfbad949257a5a4b9fdaea50ec
joey <joey.lu.au@gmail.com>
2020-01-14 11:11:47 +1100

    -fix select problem

--- src/RadioButtons.jsx
+++ src/RadioButtons.jsx
@@ -4,6 +4,7 @@ import ButtonGroup from "./ButtonGroup";
 
 const RadioButtons = ({ menus, rules }) => {
   const [disabledArray, setDisabledArray] = useState(() => {
+    // 
     let disabledArray = [];
     for (let i = 0; i < menus.length; i++) {
       disabledArray.push([]);
@@ -11,14 +12,16 @@ const RadioButtons = ({ menus, rules }) => {
         menus[i].forEach(ele =>
           disabledArray[i].push({
             id: ele.id,
-            disabled: false
+            disabled: false,
+            rule:rules[ele.id] || [] //set rule for each raio
           })
         );
       } else {
         menus[i].forEach(ele =>
           disabledArray[i].push({
             id: ele.id,
-            disabled: true
+            disabled: true,
+            rule:rules[ele.id] || []
           })
         );
       }
@@ -29,31 +32,19 @@ const RadioButtons = ({ menus, rules }) => {
   const handleClick = selectedId => {
     let currentGroup = selectedId.charAt(0) - 1;
     const result = Array.from(disabledArray);
-    result[currentGroup].map(item => {
-      //set nextDisabled
-      if (item.id !== selectedId) {
-        item.disabled = true;
-      }else{
-        if(!item.nextDisabled){
-          item.nextDisabled = rules[item.id] || [];
-        }
-      }
 
-      //set next group only if current group id is less than group number
-      if (item.nextDisabled && (currentGroup < result.length - 1)) {
-        result[currentGroup + 1].map(i => {
-          if (!item.nextDisabled.includes(parseInt(i.id))) {
-            i.disabled = false;
-            const ruleRromLastGroup = item.nextDisabled.filter(
-              j => Math.abs(j.toString()[0] - i.id.toString()[0]) > 0
-            );
-            const ownRule = rules[i.id] || [];
-            //merge rules from previous group in to current group
-            i.nextDisabled = [...ruleRromLastGroup, ...ownRule];
+    result[currentGroup].forEach(curr=>{
+      if(curr.id !== selectedId){
+        curr.disabled = true
+      }else{
+        result[currentGroup+1].forEach(next=>{
+          if(!curr.rule.includes(parseInt(next.id))){
+            next.disabled = false;
+            next.rule = [...next.rule,...curr.rule]
           }
-        });
+        })
       }
-    });
+    })
     console.log(result)
     setDisabledArray(result);
   };
